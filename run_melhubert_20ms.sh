31cfc8c05a4871372124050a1c5f45211f1f881f
tanishaagarwal03 <tanishaagarwal2007@gmail.com>
2026-01-25 21:29:41 +0000

    Change for epoch training

--- run_melhubert_20ms.sh
+++ run_melhubert_20ms.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
-#SBATCH --job-name="mel20ms"
-#SBATCH --partition=Teach-Standard
+#SBATCH --job-name="mel20ms_40ep"
+#SBATCH --partition=PGR-Standard
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
-#SBATCH --time=4:00:00
 #SBATCH --mem=16G
-#SBATCH --output=/disk/scratch/s2211921/melhubert/experiments/slurm_log/log_%j.txt
+#SBATCH --time=5-00:00:00
+#SBATCH --output=/disk/scratch/s2211921/melhubert/experiments/slurm_log/mel20ms_%j.out
 
+set -e
 
 cd /home/s2211921/MelHuBERT
 source venvmel/bin/activate
 . /home/htang2/toolchain-20251006/toolchain.rc
-mkdir -p /disk/scratch/s2211921/melhubert/experiments
+
 mkdir -p /disk/scratch/s2211921/melhubert/experiments/slurm_log
+mkdir -p /disk/scratch/s2211921/melhubert/experiments/20ms_pretrain
 
 python3 train.py \
   -f 20 \
   -g ./config/config_model_20ms.yaml \
-  -c ./config/config_runner_20ms.yaml \
-  -n /disk/scratch/s2211921/melhubert/experiments/20ms_full_run
+  -c ./config/config_runner_20ms_40ep.yaml \
+  -n /disk/scratch/s2211921/melhubert/experiments/20ms_pretrain
